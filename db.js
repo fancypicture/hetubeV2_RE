a7e1cc14649ad661e2b2507b64edc4aa2bb2b822
fancypicture <fancypicture@naver.com>
2019-09-30 03:13:58 +0900

    Added Video and Comment Model

--- db.js
+++ db.js
@@ -1,3 +1,34 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true
+  }
+); 
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = () => console.log(` Error on DB Connection: ${Error}`)
+
+db.once("open", handleOpen);
+db.on ("error", handleError);
+
+
+
+
+
+
+
+
+
+/*
 export const videos = [
     {
       id: 324393,
@@ -47,4 +78,4 @@ export const videos = [
         email: "fancypicture@gmail.com"
       }
     }
-  ];
+  ];*/
